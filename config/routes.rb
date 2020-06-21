e2e8861680c235e9e4e1aa32bdba9ce12da3d15a
mwataru-pro <m1130wata@gmail.com>
2020-06-21 22:28:30 +0900

    Add controllers & view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+  end
+  get 'post_images/index'
+  get 'post_images/show'
+  get 'post_images/new'
+  get 'users/index'
+  get 'users/show'
+  namespace :admin do
+    get 'articles/index'
+    get 'articles/show'
+    get 'articles/new'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
