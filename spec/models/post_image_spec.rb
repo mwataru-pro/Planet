645caf85ae8c01c1c8d23536917e118e7fb7f14a
mwataru-pro <m1130wata@gmail.com>
2020-08-13 14:44:47 +0900

    Add model test

--- spec/models/post_image_spec.rb
+++ spec/models/post_image_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'PostImageモデルのテスト', type: :model do
+  # タイトルを空欄で登録できない。
+  # 内容を空欄で登録できない。
+
   describe 'バリデーションのテスト' do
     let(:user) { create(:user) }
     let!(:post_image) { build(:post_image, user_id: user.id) }
@@ -24,6 +27,12 @@ RSpec.describe 'PostImageモデルのテスト', type: :model do
     context 'Userモデルとの関係' do
       it 'N:1となっている' do
         expect(PostImage.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Favoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(PostImage.reflect_on_association(:favorites).macro).to eq :has_many
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Userモデルのテスト', type: :model do
+  # 名前を空欄で登録できない。空欄で登録しようとしたらfalse。
+
  describe 'バリデーションのテスト' do
     let(:user) { build(:user) }
     subject { test_user.valid? }
@@ -13,10 +15,22 @@ RSpec.describe 'Userモデルのテスト', type: :model do
     end
  end
 
- describe 'アソシエーションのテスト' do
+  describe 'アソシエーションのテスト' do
     context 'Post_imageモデルとの関係' do
       it '1:Nとなっている' do
         expect(User.reflect_on_association(:post_images).macro).to eq :has_many
+      end
+    end
+
+    context 'Relationshipモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:active_relationships).macro).to eq :has_many
+      end
+    end
+
+    context 'Favoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:favorites).macro).to eq :has_many
       end
     end
   end
